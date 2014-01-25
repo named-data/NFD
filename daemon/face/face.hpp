2c29f3abdb5f8d92a343d2ad67b03ee5edf8ef10
Junxiao Shi <git@mail1.yoursunny.com>
2014-01-24 19:59:00 -0700

    face: declare Face class and FaceId type
    Change-Id: Iaff4e3b146d8d26de32ca745b6f9d52454d933cf

---
+++ daemon/face/face.hpp
@@ -0,0 +1,28 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/**
+ * Copyright (C) 2014 Named Data Networking Project
+ * See COPYING for copyright and distribution information.
+ */
+
+#ifndef NFD_FACE_FACE_H
+#define NFD_FACE_FACE_H
+
+#include "common.hpp"
+
+namespace ndn {
+
+/** \class FaceId
+ *  \brief identifies a face
+ */
+typedef int FaceId;
+
+/** \class Face
+ *  \brief represents a face
+ */
+class Face : noncopyable
+{
+};
+
+} // namespace ndn
+
+#endif // NFD_FACE_FACE_H
---
+++ tests/face/face.cpp
@@ -0,0 +1,19 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/**
+ * Copyright (C) 2014 Named Data Networking Project
+ * See COPYING for copyright and distribution information.
+ */
+
+#include "face/face.hpp"
+
+#include <boost/test/unit_test.hpp>
+
+namespace ndn {
+
+BOOST_AUTO_TEST_SUITE(FaceFace)
+
+// no unit test here: this ensures face.hpp can compile on its own
+
+BOOST_AUTO_TEST_SUITE_END()
+
+} // namespace ndn
