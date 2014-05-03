262f53159eca34fd6281ed4a6a566ad6ce5b9541
Junxiao Shi <git@mail1.yoursunny.com>
2014-05-03 10:45:12 -0700

    tools: create identity if not exists, obtain superuser privilege in nfd-start
    refs #1570
    
    Change-Id: I02c4c050d30d29df2b39f3b0c7dd111e6fc53a1f

--- tools/nfd-start.sh
+++ tools/nfd-start.sh
@@ -18,6 +18,17 @@ then
   exit 1
 fi
 
+if ! ndnsec-get-default &>/dev/null
+then
+  ndnsec-keygen /localhost/operator | ndnsec-install-cert -
+fi
+
+if ! sudo true
+then
+  echo 'Unable to obtain superuser privilege'
+  exit 2
+fi
+
 sudo nfd &
 sleep 2
 nrd &
