f86b5c44ae089afec555a8cb9ee16c1eccb44dda
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2014-06-13 14:04:42 -0700

    build: Disable `unneeded-internal-declaration` warning
    Change-Id: I48c99d7a1c224faebc63339d40f700a9a675d2da
    Refs: #1588

--- .waf-tools/default-compiler-flags.py
+++ .waf-tools/default-compiler-flags.py
@@ -21,7 +21,7 @@ def configure(conf):
     else:
         defaultFlags += ['-std=c++03']
 
-    defaultFlags += ['-pedantic', '-Wall', '-Wno-long-long']
+    defaultFlags += ['-pedantic', '-Wall', '-Wno-long-long', '-Wno-unneeded-internal-declaration']
 
     if conf.options.debug:
         conf.define('_DEBUG', 1)
