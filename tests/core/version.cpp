6b597e7e2319f62589ceca449eb6d947233f2d02
Junxiao Shi <git@mail1.yoursunny.com>
2014-06-29 23:41:17 -0700

    core: bump version to 0.2.0
    refs #1709
    
    Change-Id: I52dadd87ebc1f36f2b48070a4e809f07716369d7

--- tests/core/version.cpp
+++ tests/core/version.cpp
@@ -1,11 +1,12 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /**
- * Copyright (c) 2014  Regents of the University of California,
- *                     Arizona Board of Regents,
- *                     Colorado State University,
- *                     University Pierre & Marie Curie, Sorbonne University,
- *                     Washington University in St. Louis,
- *                     Beijing Institute of Technology
+ * Copyright (c) 2014,  Regents of the University of California,
+ *                      Arizona Board of Regents,
+ *                      Colorado State University,
+ *                      University Pierre & Marie Curie, Sorbonne University,
+ *                      Washington University in St. Louis,
+ *                      Beijing Institute of Technology,
+ *                      The University of Memphis
  *
  * This file is part of NFD (Named Data Networking Forwarding Daemon).
  * See AUTHORS.md for complete list of NFD authors and contributors.
@@ -20,7 +21,7 @@
  *
  * You should have received a copy of the GNU General Public License along with
  * NFD, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
- **/
+ */
 
 #include "version.hpp"
 #include "core/logger.hpp"
@@ -48,6 +49,8 @@ BOOST_AUTO_TEST_CASE(VersionString)
   NFD_LOG_INFO("NFD_VERSION_STRING " << NFD_VERSION_STRING);
 
   BOOST_STATIC_ASSERT(NFD_VERSION_MAJOR < 1000);
+  BOOST_STATIC_ASSERT(NFD_VERSION_MINOR < 1000);
+  BOOST_STATIC_ASSERT(NFD_VERSION_PATCH < 1000);
   char buf[12];
   snprintf(buf, sizeof(buf), "%d.%d.%d", NFD_VERSION_MAJOR, NFD_VERSION_MINOR, NFD_VERSION_PATCH);
 
