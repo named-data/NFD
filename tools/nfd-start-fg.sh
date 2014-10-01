b7150ca2cd2b914e20e80148057f177e2fa2c8cb
Felix Rabe <felix@rabe.io>
2014-10-01 22:19:24 +0200

    tools: Add nfd-start-fg.sh
    This is e.g. for Docker, where (by convention and best practice) one container
    should really only run one process.

---
+++ tools/nfd-start-fg.sh
@@ -0,0 +1,27 @@
+#!@BASH@
+
+VERSION="@VERSION@"
+
+case "$1" in
+  -h)
+    echo Usage
+    echo $0
+    echo "  Start NFD and RIB Management daemon and stay in the foreground"
+    exit 0
+    ;;
+  -V)
+    echo $VERSION
+    exit 0
+    ;;
+  "") ;; # do nothing
+  *)
+    echo "Unrecognized option $1"
+    exit 1
+    ;;
+esac
+
+nfd-start || exit $?
+
+read -p 'To stop, hit ENTER or CTRL-C '
+
+nfd-stop || exit $?
