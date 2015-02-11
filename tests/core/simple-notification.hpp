95ff7112aa14885c9bbadfb74c7f84952e0d4b29
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2015-02-10 19:11:33 -0800

    tests: Fix compilation due to refactoring of EncodingImpl in ndn-cxx library
    Change-Id: I3b96dd01fef810dfd2cd57d910a46f137a13879e
    Refs: #2494

--- tests/core/simple-notification.hpp
+++ tests/core/simple-notification.hpp
@@ -1,12 +1,12 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /**
- * Copyright (c) 2014,  Regents of the University of California,
- *                      Arizona Board of Regents,
- *                      Colorado State University,
- *                      University Pierre & Marie Curie, Sorbonne University,
- *                      Washington University in St. Louis,
- *                      Beijing Institute of Technology,
- *                      The University of Memphis
+ * Copyright (c) 2014-2015,  Regents of the University of California,
+ *                           Arizona Board of Regents,
+ *                           Colorado State University,
+ *                           University Pierre & Marie Curie, Sorbonne University,
+ *                           Washington University in St. Louis,
+ *                           Beijing Institute of Technology,
+ *                           The University of Memphis.
  *
  * This file is part of NFD (Named Data Networking Forwarding Daemon).
  * See AUTHORS.md for complete list of NFD authors and contributors.
@@ -53,10 +53,10 @@ public:
   wireEncode() const
   {
     ndn::EncodingBuffer buffer;
-    prependByteArrayBlock(buffer,
-                          0x8888,
-                          reinterpret_cast<const uint8_t*>(m_message.c_str()),
-                          m_message.size());
+    ndn::prependByteArrayBlock(buffer,
+                               0x8888,
+                               reinterpret_cast<const uint8_t*>(m_message.c_str()),
+                               m_message.size());
     return buffer.block();
   }
 
