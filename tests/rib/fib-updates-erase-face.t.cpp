410064619292ca9551c9fdbf0aaca02061df3b54
Junxiao Shi <git@mail1.yoursunny.com>
2016-01-19 07:31:59 -0700

    Update copyright notices for files modified in 2016
    refs #3415
    
    Change-Id: I187c4709a40d3540e89a59cf678e53cf35ad1676

--- tests/rib/fib-updates-erase-face.t.cpp
+++ tests/rib/fib-updates-erase-face.t.cpp
@@ -1,6 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /**
- * Copyright (c) 2014-2015,  Regents of the University of California,
+ * Copyright (c) 2014-2016,  Regents of the University of California,
  *                           Arizona Board of Regents,
  *                           Colorado State University,
  *                           University Pierre & Marie Curie, Sorbonne University,
