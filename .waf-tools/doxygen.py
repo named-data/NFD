dd7e5da0160dae910877653cb6153aebc410660d
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2016-03-02 15:33:20 -0800

    build: Fix compatibility with Python 3
    Change-Id: I7a1b98c38c2ed3a147c7b0e6c7b79d2723f2a3ba
    Refs: #3493

--- .waf-tools/doxygen.py
+++ .waf-tools/doxygen.py
@@ -85,7 +85,7 @@ class doxygen(Task.Task):
 
 			# Override with any parameters passed to the task generator
 			if getattr(self.generator, 'pars', None):
-				for k, v in self.generator.pars.iteritems():
+				for k, v in self.generator.pars.items():
 					self.pars[k] = v
 
 			self.doxy_inputs = getattr(self, 'doxy_inputs', [])
