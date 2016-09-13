0a5e5e2fa50bfaee5f77246f26747d0743af774e
Davide Pesavento <davide.pesavento@lip6.fr>
2016-09-13 18:29:33 +0000

    ci: fix gcovr invocation
    Change-Id: If74633cfb67725b43a55d7b190b178137ad2c420
    Refs: #3418

--- .jenkins.d/30-coverage.sh
+++ .jenkins.d/30-coverage.sh
@@ -6,9 +6,9 @@ JDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source "$JDIR"/util.sh
 
 if [[ $JOB_NAME == *"code-coverage" ]]; then
-    gcovr --output=coverage.xml \
-          --filter="$PWD/core" --filter="$PWD/daemon" --filter="$PWD/rib" --filter="$PWD/tools" \
+    gcovr --object-directory=build \
+          --output=build/coverage.xml \
+          --exclude="$PWD/(build|tests|websocketpp)" \
           --root=. \
-          --xml \
-          build
+          --xml
 fi
