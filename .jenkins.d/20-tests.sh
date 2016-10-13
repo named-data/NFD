7de0444d06ccb0af10a9647e2f3e567d3605fa0f
Davide Pesavento <davide.pesavento@lip6.fr>
2016-10-13 14:41:20 +0000

    ci: ensure XML test reports do not clobber the output of previous runs
    This was broken since commit e0edc4c06d7218900ab863b2367114a230f4e534
    
    Change-Id: I369e9ad2cb3345ef3a707799e86c3c3beaf83788
    Refs: #3418, #3750

--- .jenkins.d/20-tests.sh
+++ .jenkins.d/20-tests.sh
@@ -18,13 +18,12 @@ ndnsec-keygen "/tmp/jenkins/$NODE_NAME" | ndnsec-install-cert -
 BOOST_VERSION=$(python -c "import sys; sys.path.append('build/c4che'); import _cache; print(_cache.BOOST_VERSION_NUMBER);")
 
 ut_log_args() {
-    ((ut_count+=1))
     if (( BOOST_VERSION >= 106200 )); then
-        echo --logger=HRF,test_suite,stdout:XML,all,build/xunit-report-${ut_count}.xml
+        echo --logger=HRF,test_suite,stdout:XML,all,build/xunit-${1:-report}.xml
     else
         if [[ -n $XUNIT ]]; then
             echo --log_level=all $( (( BOOST_VERSION >= 106000 )) && echo -- ) \
-                 --log_format2=XML --log_sink2=build/xunit-report-${ut_count}.xml
+                 --log_format2=XML --log_sink2=build/xunit-${1:-report}.xml
         else
             echo --log_level=test_suite
         fi
@@ -43,15 +42,13 @@ ASAN_OPTIONS+=":strip_path_prefix=${PWD}/"
 export ASAN_OPTIONS
 
 # First run all tests as unprivileged user
-./build/unit-tests-core $(ut_log_args)
-./build/unit-tests-daemon $(ut_log_args)
-./build/unit-tests-rib $(ut_log_args)
-./build/unit-tests-tools $(ut_log_args)
+./build/unit-tests-core $(ut_log_args core)
+./build/unit-tests-daemon $(ut_log_args daemon)
+./build/unit-tests-rib $(ut_log_args rib)
+./build/unit-tests-tools $(ut_log_args tools)
 
 # Then use sudo to run those tests that need superuser powers
-sudo ./build/unit-tests-core -t TestPrivilegeHelper $(ut_log_args)
-sudo ./build/unit-tests-daemon -t Face/TestEthernetFactory,TestEthernetTransport $(ut_log_args)
-sudo ./build/unit-tests-daemon -t Mgmt/TestGeneralConfigSection/UserAndGroupConfig,NoUserConfig $(ut_log_args)
-sudo ./build/unit-tests-daemon -t Mgmt/TestFaceManager/ProcessConfig/ProcessSectionUdp,ProcessSectionUdpMulticastReinit,ProcessSectionEther,ProcessSectionEtherMulticastReinit $(ut_log_args)
-
-unset ut_count
+sudo ./build/unit-tests-core -t TestPrivilegeHelper $(ut_log_args core-privilege)
+sudo ./build/unit-tests-daemon -t Face/TestEthernetFactory,TestEthernetTransport $(ut_log_args daemon-ethernet)
+sudo ./build/unit-tests-daemon -t Mgmt/TestGeneralConfigSection/UserAndGroupConfig,NoUserConfig $(ut_log_args daemon-user-config)
+sudo ./build/unit-tests-daemon -t Mgmt/TestFaceManager/ProcessConfig/ProcessSectionUdp,ProcessSectionUdpMulticastReinit,ProcessSectionEther,ProcessSectionEtherMulticastReinit $(ut_log_args daemon-face-manager-config)
