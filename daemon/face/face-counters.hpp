6c8c2a0b9c6d1664a9ee1739ed03a340c305bd86
Ju Pan <jupan1994@gmail.com>
2019-09-04 08:01:13 +0000

    face: add a per face counter for Interests kept by the forwarder
    Refs: #4931
    Change-Id: Ica1bcec02309eff4d28a55983f089355eb3096fb

--- daemon/face/face-counters.hpp
+++ daemon/face/face-counters.hpp
@@ -1,6 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2014-2017,  Regents of the University of California,
+ * Copyright (c) 2014-2019,  Regents of the University of California,
  *                           Arizona Board of Regents,
  *                           Colorado State University,
  *                           University Pierre & Marie Curie, Sorbonne University,
@@ -75,6 +75,7 @@ public:
   const PacketCounter& nOutData;
   const PacketCounter& nInNacks;
   const PacketCounter& nOutNacks;
+  PacketCounter nKeptInterests;
 
   const PacketCounter& nInPackets;
   const PacketCounter& nOutPackets;
